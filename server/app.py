153c8581145ea44f738d8e470653b7b0ca6023a7
mwaura mwangi <fmwauramwangi@gmail.com>
2025-06-09 15:44:23 +0300

    app.py

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ def index():
     return '<h1>Welcome to my page!</h1>'
 
 
-@app.route('/<username>')
+@app.route('/<string:username>')
 def user(username):
     return f'<h1>Profile for {username}</h1>'
 
